23b3fef5d44d0e1ba72501d7c7dd646a7deb7ff8
Technological Consulting <alex.barre@technologicalconsulting.com>
2025-07-01 15:57:48 -0500

    migrate CRA to Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/AppRouter";
 
@@ -11,9 +11,8 @@ import "react-calendar/dist/Calendar.css";
 import "./assets/css/style.css";
 import "./assets/css/responsive.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppRouter />
-  </BrowserRouter>,
-  document.querySelector("#root")
+  </BrowserRouter>
 );
